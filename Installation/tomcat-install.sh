d43eb55f42fc00a6194b57fd212cbbd51ce846df
Nditafon Hyson <106729779+Hyson-Wayne@users.noreply.github.com>
2024-11-26 22:23:36 +0100

    Update tomcat-install.sh

--- Installation/tomcat-install.sh
+++ Installation/tomcat-install.sh
@@ -8,18 +8,18 @@ cd /opt
 sudo yum install git wget unzip java-17-openjdk-devel -y
 
 # Step 2: Download and Extract Tomcat
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.33/bin/apache-tomcat-10.1.33.zip
-sudo unzip apache-tomcat-10.1.33.zip
-sudo rm -rf apache-tomcat-10.1.33.zip
-sudo mv apache-tomcat-10.1.33 tomcat10
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.zip
+sudo unzip apache-tomcat-9.0.97.zip
+sudo rm -rf apache-tomcat-9.0.97.zip
+sudo mv apache-tomcat-9.0.97 tomcat9
 
 # Step 3: Set Up Tomcat Directory and Permissions
-sudo chmod 777 -R /opt/tomcat10
-sudo chown ec2-user -R /opt/tomcat10
+sudo chmod 777 -R /opt/tomcat9
+sudo chown ec2-user -R /opt/tomcat9
 
 # Step 4: Start Tomcat and Create Soft Links
 sh /opt/tomcat10/bin/startup.sh
-sudo ln -s /opt/tomcat10/bin/startup.sh /usr/bin/starttomcat
-sudo ln -s /opt/tomcat10/bin/shutdown.sh /usr/bin/stoptomcat
+sudo ln -s /opt/tomcat9/bin/startup.sh /usr/bin/starttomcat
+sudo ln -s /opt/tomcat9/bin/shutdown.sh /usr/bin/stoptomcat
 
 echo "Apache Tomcat installation and setup complete. Use 'starttomcat' to start Tomcat and 'stoptomcat' to stop it."
